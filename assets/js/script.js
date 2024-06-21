a4c3fc9edb8c396cd8a50e90e52bdc7de3b95de1
unknown <travihay@gmail.com>
2024-06-20 22:05:46 -0500

    made changes to java scrip added api keys

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,16 @@
- const apiKey = "441e8e76a168da10c7a3bb9b4464a698"
+ const apiKeyTmdb = "441e8e76a168da10c7a3bb9b4464a698"
 
- function getApidata (images) {
- const movieApi = `https://api.themoviedb.org/3/movie/157336/images?api_key=${apiKey}`;
+
+ function getApiDataTmdb () {
+ const tmdbUrl = `https://api.themoviedb.org/3/movie/157336/images?api_key=${apiKeyTmdb}`;
 
 
 console.log(movieApi);
- }
+ };
+
+
+ const apiKeyOmdb = "311fbec3"
+ 
+ function geApiDataOmdb () {
+    const omdbUrl = `http://www.omdbapi.com/?apikey=${311fbec3}&t=${title}&y=${year}`;
+ };
