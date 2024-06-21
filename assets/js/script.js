10cd760fc8ba07783186d124aee5595790bd9858
unknown <travihay@gmail.com>
2024-06-20 20:19:45 -0500

    added h2 element with title

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+ const apiKey = "441e8e76a168da10c7a3bb9b4464a698"
+
+ function getApidata (images) {
+ const movieApi = `https://api.themoviedb.org/3/movie/157336/images?api_key=${apiKey}`;
+
+
+console.log(movieApi);
+ }
